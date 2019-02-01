f285e343f28af0df520b50c4e6e6413b4c2258ef
mopo3Qo <nikolaybaskov633@gmail.com>
2019-02-01 23:29:21 +0300

    create main view

---
+++ Minsktelekomstroy/Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace Minsktelekomstroy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<MenuItem> menu = new List<MenuItem>();
+
+            menu.Add(new MenuItem("Главное меню", PackIconKind.Menu));
+            menu.Add(new MenuItem("Отделов кадров", PackIconKind.People));
+            menu.Add(new MenuItem("Услуги", PackIconKind.ServiceToolbox));
+            menu.Add(new MenuItem("О компании", PackIconKind.About));
+            menu.Add(new MenuItem("Справочная система", PackIconKind.Help));
+            menu.Add(new MenuItem("Выход", PackIconKind.Close));
+            ListViewMenu.ItemsSource = menu;
+        }
+    }
+}
