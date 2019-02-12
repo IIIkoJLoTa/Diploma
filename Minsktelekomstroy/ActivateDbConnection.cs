88bc0df78ce2f95dfd15bdb8b97eaf84ba1ead91
mopo3Qo <nikolaybaskov633@gmail.com>
2019-02-12 21:09:18 +0300

    add class to activate db connection

---
+++ Minsktelekomstroy/ActivateDbConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Minsktelekomstroy
+{
+    internal class ActivateDbConnection
+    {
+        protected internal SqlConnection connection;
+
+        public ActivateDbConnection() { }
+
+        ~ActivateDbConnection() { }
+
+        public async Task SetConnection()
+        {
+            string connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Minsktelekomstroy.mdf;Integrated Security=True";
+            using (connection = new SqlConnection(connectionstring))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
